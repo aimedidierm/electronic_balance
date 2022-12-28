2d7a63ab6bc59096db32ec959144f54d83f2f935
aimedidierm <aimedidiermugisha@gmail.com>
2022-12-28 15:21:47 +0200

    Req

--- harware/full/full.ino
+++ harware/full/full.ino
@@ -1,34 +1,72 @@
-#include <LiquidCrystal.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 #include <ArduinoJson.h>
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+LiquidCrystal_I2C lcd(0x27,20,4);
+#endif
+//pins:
+const int HX711_dout = 4; //mcu > HX711 dout pin
+const int HX711_sck = 5; //mcu > HX711 sck pin
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
 const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-String data="";
+//LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+String data = "";
 const byte ROWS = 4;
 const byte COLS = 4;
-char newNum[12]="",amount[12]="",number[12]="";
+char newNum[12] = "", amount[12] = "", number[12] = "";
 char keys[ROWS][COLS] = {
 
-    {'1','2','3'},
+  {'1', '2', '3'},
 
-    {'4','5','6'},
+  {'4', '5', '6'},
 
-    {'7','8','9'},
+  {'7', '8', '9'},
 
-    {'*','0','#'}
+  {'*', '0', '#'}
 
 };
 
 byte rowPins[ROWS] = {7, 6, 3, 2};
 byte colPins[COLS] = {8, 9, 10};
-int mi=1;
-float mass=0;
+int mi = 1;
+float mass = 0;
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-int product=0,price=0,k=0,totalam=0;
+int product = 0, price = 0, k = 0, totalam = 0;
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin(16, 2);
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
   Serial.begin(9600);
+  LoadCell.begin();
+  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  calibrationValue = 54.61; // uncomment this if you want to set the calibration value in the sketch
+#if defined(ESP8266)|| defined(ESP32)
+  //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
+#endif
+  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
 }
 
 void loop() {
@@ -37,176 +75,199 @@ void loop() {
   enteramount();
   enterphone();
 }
-void sproduct(){
+void sproduct() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Select product");
   delay(2000);
-  while(mi>0){
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("1. Sugar");
-  lcd.setCursor(0, 1);
-  lcd.print("# Next");
-  int key = keypad.getKey();
-  if (key=='1') {
-    product=1;
-    select();
-  }
-  if (key=='#') {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("2. Rice");
-  lcd.setCursor(0, 1);
-  lcd.print("# Next");
-  while(mi>0){
-  int key = keypad.getKey();
-  if (key=='2') {
-    product=2;
-    select();
-  }
-  if (key=='#') {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("3. Beans");
-  lcd.setCursor(0, 1);
-  lcd.print("# Next");
-  if (key=='#') {
-  while(mi>0){
-  int key = keypad.getKey();
-  if (key=='3') {
-    product=3;
-    select();
-  }
-  if (key=='#') {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("4. Salt");
-  lcd.setCursor(0, 1);
-  lcd.print("5. Meat");
-  while(mi>0){
-  int key = keypad.getKey();
-  if (key=='4') {
-    product=4;
-    select();
-  }
-  if (key=='5') {
-    product=5;
-    select();
-  }
-  delay(200);}
-  }
-  delay(200);}
-  }
-  delay(200);}
-  }
-  delay(200);}
-  }
-  }
-void select(){
-  //Serial.println((String)"product="+product);
-  Serial.println((String)"{'product':" + product + "}");
-  while(k==0){
-    if (Serial.available() > 0) {
-    data = Serial.readStringUntil('\n');
-    Serial.println(data);
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(data);
-      if (root["p"]) {
-      price = root["p"];
-      break;
-      }
-    }
-  }
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Product price");
-  lcd.setCursor(0, 1);
-  lcd.print(price);
-  lcd.print("Rwf/Kg");
-  delay(3000);
-  mass=1;
-  totalam=mass*price;
-  int my=0;
-  while(my==0){
-    //my test codes
+  while (mi > 0) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print(mass);
-    lcd.print(" Kg");
+    lcd.print("1. Sugar");
     lcd.setCursor(0, 1);
-    lcd.print(totalam);
-    lcd.print(" Rwf");
+    lcd.print("# Next");
     int key = keypad.getKey();
-    if (key=='#') {
-      enteramount();
+    if (key == '1') {
+      product = 1;
+      select();
+    }
+    if (key == '#') {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("2. Rice");
+      lcd.setCursor(0, 1);
+      lcd.print("# Next");
+      while (mi > 0) {
+        int key = keypad.getKey();
+        if (key == '2') {
+          product = 2;
+          select();
+        }
+        if (key == '#') {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("3. Beans");
+          lcd.setCursor(0, 1);
+          lcd.print("# Next");
+          if (key == '#') {
+            while (mi > 0) {
+              int key = keypad.getKey();
+              if (key == '3') {
+                product = 3;
+                select();
+              }
+              if (key == '#') {
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print("4. Salt");
+                lcd.setCursor(0, 1);
+                lcd.print("5. Meat");
+                while (mi > 0) {
+                  int key = keypad.getKey();
+                  if (key == '4') {
+                    product = 4;
+                    select();
+                  }
+                  if (key == '5') {
+                    product = 5;
+                    select();
+                  }
+                  delay(200);
+                }
+              }
+              delay(200);
+            }
+          }
+          delay(200);
+        }
+      }
+      delay(200);
+    }
+  }
+}
+void select() {
+  //Serial.println((String)"product="+product);
+  Serial.println((String)"{'product':" + product + "}");
+  while (k == 0) {
+    if (Serial.available() > 0) {
+      data = Serial.readStringUntil('\n');
+      Serial.println(data);
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(data);
+      if (root["p"]) {
+        price = root["p"];
+        balance();
       }
     }
-  delay(500);
   }
-void enteramount(){
-  int i=0,j=0,m=0,x=0,s=0,k=0;
+  delay(500);
+}
+void balance() {
+    //my test codes
+    int my = 0;
+    static boolean newDataReady = 0;
+    const int serialPrintInterval = 0; //increase value to slow down serial print activity
+
+    // check for new data/start next conversion:
+    if (LoadCell.update()) newDataReady = true;
+
+    // get smoothed value from the dataset:
+    if (newDataReady) {
+      if (millis() > t + serialPrintInterval) {
+        float i = LoadCell.getData();
+        Serial.print("Load_cell output val: ");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        totalam = i * price;
+        lcd.print("Mass:");
+        lcd.print(i);
+        lcd.print("g");
+        lcd.setCursor(0, 1);
+        lcd.print(totalam);
+        lcd.print("Rwf/g");
+        newDataReady = 0;
+        t = millis();
+      }
+    }
+
+    // receive command from serial terminal, send 't' to initiate tare operation:
+    if (Serial.available() > 0) {
+      char inByte = Serial.read();
+      if (inByte == 't') LoadCell.tareNoDelay();
+    }
+
+    // check if last tare operation is complete:
+    if (LoadCell.getTareStatus() == true) {
+      Serial.println("Tare complete");
+    }
+    int key = keypad.getKey();
+    if (key == '#') {
+      enteramount();
+    }
+  }
+void enteramount() {
+  int i = 0, j = 0, m = 0, x = 0, s = 0, k = 0;
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Please enter");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("amount payed");
   delay(2000);
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Enter money");
-  lcd.setCursor(0,1);
-  for(i=2;i>0;i++){
-    lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
+  for (i = 2; i > 0; i++) {
+    lcd.setCursor(0, 1);
     int key = keypad.getKey();
-    if (key!=NO_KEY && key!='#' && key!='*'){
-        amount[j] = key;
-        amount[j+1]='\0';   
-        j++;
-        lcd.print(amount);
+    if (key != NO_KEY && key != '#' && key != '*') {
+      amount[j] = key;
+      amount[j + 1] = '\0';
+      j++;
+      lcd.print(amount);
     }
-    if (key=='#'&& j>0)
+    if (key == '#' && j > 0)
     {
       enterphone();
-      }
-      delay(100);
-      }
+    }
+    delay(100);
   }
-void enterphone(){
-  int i=0,j=0,m=0,x=0,s=0,k=0;
+}
+void enterphone() {
+  int i = 0, j = 0, m = 0, x = 0, s = 0, k = 0;
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Please enter");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("phone number");
   delay(2000);
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Enter number");
-  lcd.setCursor(0,1);
-  for(i=2;i>0;i++){
-    lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
+  for (i = 2; i > 0; i++) {
+    lcd.setCursor(0, 1);
     int key = keypad.getKey();
-    if (key!=NO_KEY && key!='#' && key!='*'){
-        number[j] = key;
-        number[j+1]='\0';   
-        j++;
-        lcd.print(number);
+    if (key != NO_KEY && key != '#' && key != '*') {
+      number[j] = key;
+      number[j + 1] = '\0';
+      j++;
+      lcd.print(number);
     }
-    if (key=='#'&& j>0)
+    if (key == '#' && j > 0)
     {
       ending();
-      }
-      delay(100);
-      }
-  }
-  void ending(){
-    //Serial.println((String)"bill="+product+"product="+product+"&phone="+number+"&total="+totalam+"&amount="+amount+"&mass="+mass);
-    
-    Serial.println((String)"{'bill':" + product + ", '" + "product':" + product + ", '" + "phone':'" + number + "', '" + "total':'" + totalam + "', '" + "amount':'" + amount + "', '" + "mass':'" + mass + "}");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Bill sent");
-    delay(4000);
-      sproduct();
     }
+    delay(100);
+  }
+}
+void ending() {
+  //Serial.println((String)"bill="+product+"product="+product+"&phone="+number+"&total="+totalam+"&amount="+amount+"&mass="+mass);
+
+  Serial.println((String)"{'bill':" + product + ", '" + "product':" + product + ", '" + "phone':'" + number + "', '" + "total':'" + totalam + "', '" + "amount':'" + amount + "', '" + "mass':'" + mass + "}");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Bill sent");
+  delay(4000);
+  sproduct();
+}
--- harware/nodemcu/nodemcu.ino
+++ harware/nodemcu/nodemcu.ino
@@ -1,35 +1,78 @@
-s#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-#include<SoftwareSerial.h>
-SoftwareSerial s(3,1);
-const char* ssid = "Virus";
-const char* password = "mbega123455";
+#include <ArduinoJson.h>
+
+const char* ssid = "Balance";
+const char* password = "balance123";
+
 String serverName = "http://137.184.232.255/electronic_balance/data.php";
-//String serverName = "http://didier.requestcatcher.com/";
 //String serverName = "http://192.168.43.76/electronic_balance/data.php";
+
 void setup() {
-  s.begin(9600);
+  Serial.begin(9600); 
+
   WiFi.begin(ssid, password);
+  Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
-  delay(500);
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
-    if(s.available( ) > 0){
+  // Send an HTTP POST request depending on timerDelay
+  if (Serial.available( ) > 0) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
       WiFiClient client;
       HTTPClient http;
-      http.begin(client, serverName);
-      //http.addHeader("Content-Type", "application/json");
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      String httpRequestData = s.readStringUntil('\n');
-      int      httpResponseCode = http.POST(httpRequestData);
-      if (httpResponseCode>0) {
-        s.println(httpResponseCode);
-        String payload = http.getString();
-        s.println(payload);
+      String data = Serial.readStringUntil('\n');
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(data);
+      String serverPath = serverName;
+      if(root["bill"]){
+      String bill = root["bill"];
+      String product = root["product"];
+      String phone = root["phone"];
+      String total = root["total"];
+      String amount = root["amount"];
+      String mass = root["mass"];
+      String serverPath = serverName + "?bill="+bill+"product="+product+"&phone="+phone+"&total="+total+"&amount="+amount+"&mass="+mass;
       }
+      if(root["product"]){
+      String product = root["product"];
+      String serverPath = serverName + "?product="+product;
+      }
+      Serial.println(serverPath);
+      http.begin(client, serverPath);
+  
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+        
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
       http.end();
     }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
   }
+}
